42429fde2de9dae6665be51d29e241bcbde76e64
王从燕 <wcy@192.168.12.105>
2017-06-20 00:49:21 +0800

    add playerinfo module

---
+++ assets/Scripts/playerInfo/EquipItem.js
@@ -0,0 +1,18 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        type:0,
+        
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
