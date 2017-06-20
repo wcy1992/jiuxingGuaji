f376afa00e6314857b82c8e595ef14f5b1b407b3
王从燕 <wcy@192.168.12.105>
2017-06-21 00:28:00 +0800

    roleinfo panel change

--- assets/Scripts/playerInfo/EquipItem.js
+++ assets/Scripts/playerInfo/EquipItem.js
@@ -3,16 +3,21 @@ cc.Class({
 
     properties: {
         type:0,
-        
+        itemType:cc.Node,
+        itemIcon:cc.Node
     },
 
     // use this for initialization
     onLoad: function () {
-
+    	
+      var sprite  = this.itemType.getComponent("cc.SpriteFrame");
+      var url = 'heroproperity/item'+this.type;
+	  cc.loader.loadRes(url, cc.SpriteFrame, (err, sprite) => 
+	  {
+	  	sprite.spriteFrame = sprite ;
+	   });
     },
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
 
-    // },
+
 });
